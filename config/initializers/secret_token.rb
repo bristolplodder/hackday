ba6b29b88e21b0d1bcc582e0b3ff79245c3701c1
OpenShift guest <mail@matthewcockburn.org.uk>
2014-03-06 08:17:30 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackday::Application.config.secret_key_base = 'f748226df1902bea8d597d450488cd899a9230ee314c37b09f5a351453966e10649cc803daa99f7903d416a1abd8c71a1428c06d2e6bfb03c4f17672609a2c49'
